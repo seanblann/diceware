b228389f26352f29b2bf2ddbfe9df0087a1badba
seanblann <sblann@cnm.edu>
2022-02-23 10:02:30 -0700

    Partial implementation of DicewareGenerator alternate

--- src/main/java/edu/cnm/deepdive/diceware/service/DicewareGenerator.java
+++ src/main/java/edu/cnm/deepdive/diceware/service/DicewareGenerator.java
@@ -4,12 +4,16 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 import java.util.stream.IntStream;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class DicewareGenerator implements PassphraseGenerator {
 
   private final Random rng;
   private final List<String> words;
 
+  @Autowired
   public DicewareGenerator(Random rng, WordProvider provider) {
     this.rng = rng;
     words = new ArrayList<>(provider.getWords());
@@ -25,6 +29,7 @@ public class DicewareGenerator implements PassphraseGenerator {
         .toArray(String[]::new);
 
     }
-  }
+
 
 }
+
